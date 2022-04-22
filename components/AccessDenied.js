79f9c087a3600a62420a129dfab51e7b0fac0e90
Edgardo Rodriguez <62724786+EdgarDev17@users.noreply.github.com>
2022-04-22 13:07:13 -0500

    I made some UI improvement to Access Denied component

--- components/AccessDenied.js
+++ components/AccessDenied.js
@@ -5,9 +5,9 @@ export default function AccessDenied() {
     return (
         <div className='h-screen flex justify-center items-center'>
             <div className='shadow-lg px-7 flex flex-col items-center justify-center'>
-                <p className='text-xl text-black font-bold text-center my-5'>¡Hey! Alto ahí 👋🏼</p>
+                <p className='text-xl text-black font-bold text-center my-5'>¡Hey! Alto ahí 🤔</p>
                 <p className='text-lg text-black text-justify my-5'>
-                    Para poder acceder a este contenido primero debes iniciar
+                    ¡Para poder acceder a este contenido primero debes ser parte de la banda! para ello debes iniciar
                     sesión o crear una cuenta.
                 </p>
                 <div className={'py-5'}>
