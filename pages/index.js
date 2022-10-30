372280e86f40250614f06f22479b87c63d35004a
Edgardo Rodriguez <62724786+EdgarDev17@users.noreply.github.com>
2022-10-30 12:12:09 -0600

    improving Login UI

--- pages/index.js
+++ pages/index.js
@@ -3,45 +3,45 @@ import { getProviders, signIn } from 'next-auth/react'
 import Image from 'next/image'
 
 export default function Login({ providers }) {
-  let provider = Object.values(providers).map((provider) => {
-    return (
-      <button
-        className={'bg-green-500 text-white px-10 py-3 rounded'}
-        key={provider.name}
-        onClick={() => {
-          return signIn(provider.id, { callbackUrl: '/home' })
-        }}
-      >
-        {provider.name}
-      </button>
-    )
-  })
+	let provider = Object.values(providers).map((provider) => {
+		return (
+			<button
+				className={'bg-green-500 text-white px-10 py-3 rounded'}
+				key={provider.name}
+				onClick={() => {
+					return signIn(provider.id, { callbackUrl: '/home' })
+				}}
+			>
+				{provider.name}
+			</button>
+		)
+	})
 
-  return (
-    <>
-      <div className={'container mx-auto'}>
-        <h1 className={'font-bold text-xl text-center my-5'}>
-          Welcome to the band, my friend!
-        </h1>
+	return (
+		<div className='bg-gray-900 w-full h-screen flex flex-col justify-center items-center'>
+			<div className={'container mx-auto'}>
+				<h1 className={' text-white font-bold text-xl sm:text-5xl text-center sm:mb-5'}>
+					Welcome to the band, my friend!
+				</h1>
 
-        <p className={'text-md my-5 text-center'}>
-          Login with your spotify account to enjoy this app!
-        </p>
-      </div>
+				<p className={'text-white text-sm my-5 sm:mt-7 sm:mb-10 md:text-lg text-center'}>
+					Login to know about your music taste
+				</p>
+			</div>
 
-      <div className={'flex justify-center flex-col mx-5 lg:mx-96'}>
-        {provider}
-      </div>
-    </>
-  )
+			<div className={'flex justify-center flex-col'}>
+				{provider}
+			</div>
+		</div>
+	)
 }
 
 export const getServerSideProps = async () => {
-  const providers = await getProviders()
+	const providers = await getProviders()
 
-  return {
-    props: {
-      providers,
-    },
-  }
+	return {
+		props: {
+			providers,
+		},
+	}
 }
