44618bea5ba9272c3ce429636952082caabdc327
Edgardo Rodriguez <62724786+EdgarDev17@users.noreply.github.com>
2022-10-30 14:54:53 -0600

    updating env variables

--- components/AccessDenied.js
+++ components/AccessDenied.js
@@ -5,13 +5,12 @@ export default function AccessDenied() {
     return (
         <div className='h-screen flex justify-center items-center'>
             <div className='shadow-lg px-7 flex flex-col items-center justify-center'>
-                <p className='text-xl text-black font-bold text-center my-5'>¡Hey! Alto ahí 🤔</p>
+                <p className='text-xl text-black font-bold text-center my-5'>Hello there!</p>
                 <p className='text-lg text-black text-justify my-5'>
-                    ¡Para poder acceder a este contenido primero debes ser parte de la banda! para ello debes iniciar
-                    sesión o crear una cuenta.
+                    To enjoy this website, you need to be logged in
                 </p>
                 <div className={'py-5'}>
-                    <LinkButton url={'/'} label={'Iniciar Sesión'}/>
+                    <LinkButton url={'/'} label={'Login page'}/>
                 </div>
             </div>
         </div>
